b4995f6a19471b5e1588dbee0d5dcca0589a1f3c
unknown <socrates@.(none)>
2011-09-03 20:19:50 +0800

    add with_statement for python 2.5

--- riak/transports/pbc.py
+++ riak/transports/pbc.py
@@ -17,6 +17,7 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 """
+from __future__ import with_statement
 import socket, struct
 
 try:
